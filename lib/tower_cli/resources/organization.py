9ab086c8fd5e93ac02e67fb83da357b47d96c609
Aaron Tan <sitan@sitan-OSX.local>
2016-06-24 15:24:21 -0400

    Solve issue #159

--- lib/tower_cli/resources/organization.py
+++ lib/tower_cli/resources/organization.py
@@ -27,43 +27,49 @@ class Resource(models.Resource):
     description = models.Field(required=False, display=False)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--user', type=types.Related('user'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--user', type=types.Related('user'), required=True)
     def associate(self, organization, user):
         """Associate a user with this organization."""
         return self._assoc('users', organization, user)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--user', type=types.Related('user'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--user', type=types.Related('user'), required=True)
     def associate_admin(self, organization, user):
         """Associate an admin with this organization."""
         return self._assoc('admins', organization, user)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--user', type=types.Related('user'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--user', type=types.Related('user'), required=True)
     def disassociate(self, organization, user):
         """Disassociate a user from this organization."""
         return self._disassoc('users', organization, user)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--user', type=types.Related('user'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--user', type=types.Related('user'), required=True)
     def disassociate_admin(self, organization, user):
         """Disassociate an admin from this organization."""
         return self._disassoc('admins', organization, user)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--project', type=types.Related('project'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--project', type=types.Related('project'), required=True)
     def associate_project(self, organization, project):
         """Associate a project with this organization."""
         return self._assoc('projects', organization, project)
 
     @resources.command(use_fields_as_options=False)
-    @click.option('--organization', type=types.Related('organization'))
-    @click.option('--project', type=types.Related('project'))
+    @click.option('--organization', type=types.Related('organization'),
+                  required=True)
+    @click.option('--project', type=types.Related('project'), required=True)
     def disassociate_project(self, organization, project):
         """Disassociate a project from this organization."""
         return self._disassoc('projects', organization, project)
